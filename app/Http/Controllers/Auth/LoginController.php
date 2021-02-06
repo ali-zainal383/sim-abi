801f6574a4bd32b16dd43beb996817d277dc7ccc
ali-zainal383 <ali.zainal312@gmail.com>
2021-02-06 11:12:47 +0700

    fix space

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -41,7 +41,6 @@ class LoginController extends Controller
     public function __construct()
     {
         if (Auth::check() && Auth::user()->role->id == 1) {
-
             $this->redirectTo = route('admin.dashboard');
         } else {
             $this->redirectTo = route('ketua.dashboard');
