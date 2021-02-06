912769b1b69a9760044e9f40874d8b4b81cd870c
ali-zainal383 <ali.zainal312@gmail.com>
2021-02-06 12:43:54 +0700

    add csrf toke

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,7 @@
    <title>DPD ABI Kab.Malang | Log in</title>
    <!-- Tell the browser to be responsive to screen width -->
    <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta name="csrf-token" content="{{ csrf_token() }}">
 
    <!-- Font Awesome -->
    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css') }}">
@@ -25,6 +26,7 @@
    <div class="login-box">
       <div class="login-logo">
          <a href="#">SIM Anggota ABI</a>
+         
       </div>
       <!-- /.login-logo -->
       <div class="card">
