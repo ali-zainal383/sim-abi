72fbb989b86974112ee1e34216d02c6a28924d68
ali-zainal383 <ali.zainal312@gmail.com>
2021-02-12 12:44:39 +0700

    edit Dashboard and create User Controller

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,17 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
 
-class DashboardController extends Controller
+class UserController extends Controller
 {
     public function index()
     {
         $users = User::all();
-        $levels = Role::all();
 
-        return view('admin.dashboard', compact('users', 'levels'));
+        return view('admin.user.index', compact('users'));
     }
 }
